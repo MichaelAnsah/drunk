662719c708ce92e1ba6ce863abf57e223a2cfc1a
Michael Kwame <Michael Kwame>
2017-01-18 15:18:32 +0000

    Hensonhrm file

---
+++ hensonhrm/hensonhrm/symfony/apps/orangehrm/templates/_copyright.php
@@ -0,0 +1,8 @@
+<?php 
+$version = '3.3.3';
+$prodName = 'OrangeHRM';
+$copyrightYear = date('Y');
+
+?>
+<?php /*echo $prodName . ' ' . $version;*/ ?><br/>
+&copy;<?php echo $copyrightYear;?> <a href="http://www.hensongeodata.com" target="_blank">Henson Geodata Technologies</a>. All rights reserved.
